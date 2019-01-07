4388c47796cc096e69933781bdfe9ac0421221f7
JamieHo2018 <jamie-ho2009@hotmail.com>
2019-01-07 06:56:46 -0800

    totally forget your existance

---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Grill Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Salmon Burger');
